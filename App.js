e71a3159a6d3dcedd710ae53e7c5596e1c8346f2
chavante01 <86327521+chavante01@users.noreply.github.com>
2022-10-23 11:15:14 -0400

    finalizando cronometro
    adicionei todas as funcionalidades desejadas

--- App.js
+++ App.js
@@ -2,25 +2,64 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, ImageBackground, TouchableOpacity } from 'react-native';
 
 export default class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      numero:0,
+      botaocomecar:'Começar',
+      ultimo: null,
+    };
+    this.timer = null; //variável do timer 
+
+    this.contar = this.contar.bind(this);
+    this.zerar = this.zerar.bind(this);
+  }
+
+  contar(){
+    if(this.timer != null){         //função responsável por parar o cronometro e limpar os dados.
+      clearInterval(this.timer);
+      this.timer = null;
+
+      this.setState({botaocomecar: 'Começar'});
+    }else{
+      this.timer = setInterval(() => this.setState({numero: this.state.numero + 0.1}) , 100);
+
+      this.setState({botaocomecar: 'Parar'});
+    }
+  }
+
+  zerar(){
+    if(this.timer != null){
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+    this.setState({
+      ultimo: this.state.numero,
+      numero:0,
+      botao: 'Começar',     
+    })
+  }
+
+
   render(){
     return (
       <View style={styles.container}>
         <ImageBackground style={styles.cronometro} source={require('./assets/cronometro.png')} resizeMode='cover'>
-          <Text style={styles.contador}>0.0</Text> 
+          <Text style={styles.contador}>{this.state.numero.toFixed(1)}</Text> 
         </ImageBackground>
 
         <View style={styles.areabotoes}>
-          <TouchableOpacity style={styles.botao}>
-            <Text style={styles.textodobotao}>Começar</Text>
+          <TouchableOpacity style={styles.botao} onPress={this.contar}>
+            <Text style={styles.textodobotao}>{this.state.botaocomecar}</Text>
           </TouchableOpacity>
 
           <TouchableOpacity style={styles.botao}>
-            <Text style={styles.textodobotao}>Zerar</Text>
+            <Text style={styles.textodobotao} onPress={this.zerar}>Zerar</Text>
           </TouchableOpacity>
         </View>
         
         <View style={styles.arearegistro}>
-          <Text style={styles.registro}> Último tempo: </Text>
+          <Text style={styles.registro}>{this.state.ultimo > 0 ? 'Ultimo tempo: ' + this.state.ultimo.toFixed(1) + 's' : ''} </Text>
         </View> 
         
       </View>
@@ -73,3 +112,4 @@ const styles = StyleSheet.create({
     fontSize:23,
   },
 });
+
